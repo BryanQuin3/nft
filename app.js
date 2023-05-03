686354303bb41cb9e5c2e4ba273036ed5e1e8f0f
Bryan Quintana <115370464+BryanQuin3@users.noreply.github.com>
2023-05-03 11:58:59 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
+// Menu
 const boton = document.querySelector("#boton");
 const menu = document.querySelector("#menu");
 boton.addEventListener("click", () => {
   menu.classList.toggle("hidden");
 });
 
-const url = "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD";
-const spanPrecio = document.querySelector("#usd");
-
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    spanPrecio.textContent = `${data.USD} $`;
-  })
-  .catch((error) => {
-    console.error(error);
-  });
 // Scroll
 const scrollContainer = document.querySelector(".scroll-container");
-// distancia a desplazar
 const scrollDistance = 125;
 const scrollLeftBtn = document.querySelector(".scroll-left-btn");
 const scrollRightBtn = document.querySelector(".scroll-right-btn");
@@ -26,7 +15,38 @@ scrollLeftBtn.addEventListener("click", () => {
   scrollContainer.scrollBy(-scrollDistance, 0);
 });
 
-
 scrollRightBtn.addEventListener("click", () => {
   scrollContainer.scrollBy(scrollDistance, 0);
+});
+
+// Sellers
+const divSellers = document.querySelector("#sellers");
+let sellers = ["Elizabeth","Christy H","Jhoan Pen","Lila Komp","Kishimoto","Collins","Carmen P","Amada B"
+  ,"Christy H","Jhoan Pen","Lila Komp","Kishimoto"];
+function sellersCards (precio){
+  for (let i = 0; i < sellers.length; i++) {
+    let random = Math.random()*100;
+    let volumen = Math.round(random*precio)
+    let sellerImg = `./img/seller${i+1}.png`;
+    let sellerName = `${sellers[i]}`;
+    let divCardSeller = `
+    <div class="grid place-items-center flex-shrink-0">
+      <img class="" src="${sellerImg}" alt="">
+      <h4 class="text-lg text-white  font-semibold lg:text-xl">${sellerName}</h4>
+      <span class="text-blue-500" id="#volumen">${volumen.toLocaleString()} USD</span>
+    `;
+    divSellers.innerHTML += divCardSeller;
+  }
+}
+
+// Api
+const url = "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD";
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    precio = data.USD;
+    sellersCards(precio);
+  })
+  .catch((error) => {
+    console.error(error);
 });
